8fac01a3118b9228855236d6fa76d4eb07e517b2
Nambukeerthi <nambu935@gmail.com>
2024-12-17 00:07:12 +0530

    Update copper.py

--- copper.py
+++ copper.py
@@ -22,9 +22,9 @@ with st.sidebar:
 class option():
   country_values = [28.,  25.,  30.,  32.,  38.,  78.,  27.,  77., 113.,  79.,  26., 39.,  40.,  84.,  80., 107.,  89.]
   status_values = ['Won', 'Draft', 'To be approved', 'Lost', 'Not lost for AM', 'Wonderful', 'Revised', 'Offered', 'Offerable']
-  status encoded = ['Won':0, 'Draft':1, 'To be approved':2, 'Lost':3, 'Not lost for AM':4, 'Wonderful':5, 'Revised':6, 'Offered':7, 'Offerable':8]
+  #status encoded = ['Won':0, 'Draft':1, 'To be approved':2, 'Lost':3, 'Not lost for AM':4, 'Wonderful':5, 'Revised':6, 'Offered':7, 'Offerable':8]
   item_type_values = ['W', 'WI', 'S', 'Others', 'PL', 'IPL', 'SLAWR']
-  item_type_encoded = ['W':0.0, 'WI':1.0, 'S':2.0, 'Others':3.0, 'PL':4.0, 'IPL':5.0, 'SLAWR':6.0]
+  #item_type_encoded = ['W':0.0, 'WI':1.0, 'S':2.0, 'Others':3.0, 'PL':4.0, 'IPL':5.0, 'SLAWR':6.0]
   application_values = [10.0, 41.0, 28.0, 59.0, 15.0, 4.0, 38.0, 56.0, 42.0, 26.0, 27.0, 19.0, 20.0, 66.0, 29.0, 22.0, 40.0, 25.0, 67.0, 79.0, 3.0, 99.0,  2.0,  5.0, 39.0, 69.0, 70.0, 65.0, 58.0, 68.0]
   product_ref_values = [1670798778, 1668701718,     628377,     640665,     611993, 
        1668701376,  164141591, 1671863738, 1332077137,     640405,
