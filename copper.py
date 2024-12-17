2afee12d6c7ced97c9ab001924e8dea48fa29029
Nambukeerthi <nambu935@gmail.com>
2024-12-17 22:06:07 +0530

    Update copper.py

--- copper.py
+++ copper.py
@@ -99,15 +99,15 @@ elif selected == "Prediction":
       
               with st.form("my form"): 
                      
-                         col1,col2,col3 = st.columns([5,2,5])
-                      with col1:
+                         col4,col5,col6 = st.columns([5,2,5])
+                      with col4:
                                  st.write (" ")
                                  status =  st.selectbox("Status",status_values,key =1)
                                  item_type =  st.selectbox("Item Type",item_type_values, key =2)
                                  country =  st.selectbox("Country",country_values, key =3)
                                  application =  st.selectbox("Application",application_values, key =4)
                                  product_ref =  st.selectbox("Product Reference",product_ref_values, key =5)
-                      with col3:
+                      with col6:
                                  #st.write(f'<h5 style="color:rgb(0, 153, 153, 0.4);">NOTE: Min & Max given for reference, y)
                                  quantity_tons = st.text_input("Enter Quantity Tons (Min:611728 & Max:1722207579)")
                                  thickness = st.text_input("Enter Thickness (Min:0.18 & Max:400)")
