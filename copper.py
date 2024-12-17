5197abaf920011604a0dac33aa4339f61478bf11
Nambukeerthi <nambu935@gmail.com>
2024-12-17 21:12:14 +0530

    Update copper.py

--- copper.py
+++ copper.py
@@ -90,7 +90,7 @@ elif selected == "Prediction":
                                            }
                                            </sytle>
                                            """, unsafe_allow_html=True)      
-                                     st.write("Predicting the price for: ok ")
+                                      st.write("Predicting the price for: ok ")
   
     with tab2:
       
