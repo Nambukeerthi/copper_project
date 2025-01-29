817f7fd9fffc8a4e02090dae0b5e042c519a8bd9
NAMBU KEERTHI R <nambu935@gmail.com>
2025-01-30 01:20:54 +0530

    Update copper.py

--- copper.py
+++ copper.py
@@ -86,24 +86,24 @@ elif selected == "Prediction":
 
                                       with open("models/cmodel.pkl", 'rb') as file:
                                         loaded_model = pickle.load(file)
-                                      with open("models/cscaler.pkl", 'rb') as f:
-                                        scaler_loaded = pickle.load(f)
-                                      with open("models/ct.pkl", 'rb') as f:
-                                        t_loaded = pickle.load(f)
-                                      with open(r"models/s.pkl", 'rb') as f:
-                                          s_loaded = pickle.load(f)
+                                      #with open("models/cscaler.pkl", 'rb') as f:
+                                        #scaler_loaded = pickle.load(f)
+                                      #with open("models/ct.pkl", 'rb') as f:
+                                        #t_loaded = pickle.load(f)
+                                      #with open(r"models/s.pkl", 'rb') as f:
+                                          #s_loaded = pickle.load(f)
               
                                       # -----Sending that data to the trained models for selling price prediction-----
                                       new_sample = np.array([[np.log(float(quantity_tons)), application, np.log(float(thickness)),
                                                               float(width), country, float(customer), int(product_ref), item_type,
                                                               status]])
-                                      new_sample_ohe = t_loaded.transform(new_sample[:, [7]]).toarray()
-                                      new_sample_be = s_loaded.transform(new_sample[:, [8]]).toarray()
-                                      new_sample = np.concatenate(
+                                      #new_sample_ohe = t_loaded.transform(new_sample[:, [7]]).toarray()
+                                      #new_sample_be = s_loaded.transform(new_sample[:, [8]]).toarray()
+                                      #new_sample = np.concatenate(
                                           (new_sample[:, [0, 1, 2, 3, 4, 5, 6]], new_sample_ohe, new_sample_be),
                                           axis=1)
-                                      new_sample1 = scaler_loaded.transform(new_sample)
-                                      new_pred = loaded_model.predict(new_sample1)[0]
+                                      #new_sample1 = scaler_loaded.transform(new_sample)
+                                      new_pred = loaded_model.predict(new_sample)[0]
               
                                       # Used np.log earlier to handle data discrepancies, so to get the real output using np.exp
                                       st.write('## :green[Predicted selling price:] ', np.exp(new_pred))
