9c4268e066308b5e4503206036e1dcf9f41fbf8c
NAMBU KEERTHI R <nambu935@gmail.com>
2025-02-06 23:41:35 +0530

    Update copper.py

--- copper.py
+++ copper.py
@@ -6,6 +6,13 @@ import pickle
 import html
 import json
 import sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import GridSearchCV
+from sklearn.preprocessing import LabelBinarizer
+import re
 
 st. set_page_config(
                    page_title='Indutrial Copper', 
@@ -22,20 +29,7 @@ st.markdown(" ")
 
 with st.sidebar:
   selected = option_menu('Main Menu', options=['Home', 'Prediction'], icons=['house','lightbulb'], default_index=1,orientation='vertical')
-class option():
-  country_values = [28.,  25.,  30.,  32.,  38.,  78.,  27.,  77., 113.,  79.,  26., 39.,  40.,  84.,  80., 107.,  89.]
-  status_values = ['Won', 'Draft', 'To be approved', 'Lost', 'Not lost for AM', 'Wonderful', 'Revised', 'Offered', 'Offerable']
-  #status encoded = ['Won':0, 'Draft':1, 'To be approved':2, 'Lost':3, 'Not lost for AM':4, 'Wonderful':5, 'Revised':6, 'Offered':7, 'Offerable':8]
-  item_type_values = ['W', 'WI', 'S', 'Others', 'PL', 'IPL', 'SLAWR']
-  #item_type_encoded = ['W':0.0, 'WI':1.0, 'S':2.0, 'Others':3.0, 'PL':4.0, 'IPL':5.0, 'SLAWR':6.0]
-  application_values = [10.0, 41.0, 28.0, 59.0, 15.0, 4.0, 38.0, 56.0, 42.0, 26.0, 27.0, 19.0, 20.0, 66.0, 29.0, 22.0, 40.0, 25.0, 67.0, 79.0, 3.0, 99.0,  2.0,  5.0, 39.0, 69.0, 70.0, 65.0, 58.0, 68.0]
-  product_ref_values = [1670798778, 1668701718,     628377,     640665,     611993, 
-       1668701376,  164141591, 1671863738, 1332077137,     640405,
-       1693867550, 1665572374, 1282007633, 1668701698,     628117,
-       1690738206,     628112,     640400, 1671876026,  164336407,
-        164337175, 1668701725, 1665572032,     611728, 1721130331,
-       1693867563,     611733, 1690738219, 1722207579,  929423819,
-       1665584320, 1665584662, 1665584642]
+
   
 if selected == "Home":
         st.markdown(" ")
@@ -51,96 +45,134 @@ elif selected == "Prediction":
   
     tab1,tab2 = st.tabs(["PREDICT SELLING PRICE","PREDICT STATUS"])
     with tab1:
-            country_values = [28.,  25.,  30.,  32.,  38.,  78.,  27.,  77., 113.,  79.,  26., 39.,  40.,  84.,  80., 107.,  89.]
-            status_values = ['Won', 'Draft', 'To be approved', 'Lost', 'Not lost for AM', 'Wonderful', 'Revised', 'Offered', 'Offerable']
-            item_type_values = ['W', 'WI', 'S', 'Others', 'PL', 'IPL', 'SLAWR']
-            application_values = [10.0, 41.0, 28.0, 59.0, 15.0, 4.0, 38.0, 56.0, 42.0, 26.0, 27.0, 19.0, 20.0, 66.0, 29.0, 22.0, 40.0, 25.0, 67.0, 79.0, 3.0, 99.0,  2.0,  5.0, 39.0, 69.0, 70.0, 65.0, 58.0, 68.0]
-            product_ref_values = [1670798778, 1668701718,     628377,     640665,     611993, 
-                                  1668701376,  164141591, 1671863738, 1332077137,     640405,
-                                  1693867550, 1665572374, 1282007633, 1668701698,     628117,
-                                  1690738206,     628112,     640400, 1671876026,  164336407,
-                                  164337175, 1668701725, 1665572032,     611728, 1721130331,
-                                  1693867563,     611733, 1690738219, 1722207579,  929423819,
-                                  1665584320, 1665584662, 1665584642]
-      
-            with st.form("my form 1"): 
-            
-                      col1,col2,col3 = st.columns([5,2,5])
+                       # Define the possible values for the dropdown menus
+                  status_options = ['Won', 'Draft', 'To be approved', 'Lost', 'Not lost for AM', 'Wonderful', 'Revised', 'Offered', 'Offerable']
+                  item_type_options = ['W', 'WI', 'S', 'Others', 'PL', 'IPL', 'SLAWR']
+                  country_options = [28., 25., 30., 32., 38., 78., 27., 77., 113., 79., 26., 39., 40., 84., 80., 107., 89.]
+                  application_options = [10., 41., 28., 59., 15., 4., 38., 56., 42., 26., 27., 19., 20., 66., 29., 22., 40., 25., 67., 79., 3., 99., 2., 5., 39., 69., 70., 65., 58., 68.]
+                  product=['611112', '611728', '628112', '628117', '628377', '640400', '640405', '640665', 
+                               '611993', '929423819', '1282007633', '1332077137', '164141591', '164336407', 
+                               '164337175', '1665572032', '1665572374', '1665584320', '1665584642', '1665584662', 
+                               '1668701376', '1668701698', '1668701718', '1668701725', '1670798778', '1671863738', 
+                               '1671876026', '1690738206', '1690738219', '1693867550', '1693867563', '1721130331', '1722207579']
+          
+                  # Define the widgets for user input
+                  with st.form("my_form"):
+                      col1,col2,col3=st.columns([5,2,5])
                       with col1:
-                                 st.write (" ")
-                                 status =  st.selectbox("Status",status_values,key =1)
-                                 item_type =  st.selectbox("Item Type",item_type_values, key =2)
-                                 country =  st.selectbox("Country",country_values, key =3)
-                                 application =  st.selectbox("Application",application_values, key =4)
-                                 product_ref =  st.selectbox("Product Reference",product_ref_values, key =5)
-                      with col3:
-                                 #st.write(f'<h5 style="color:rgb(0, 153, 153, 0.4);">NOTE: Min & Max given for reference, y)
-                                 quantity_tons = st.number_input("Enter Quantity Tons (Min:611728 & Max:1722207579)")
-                                 thickness = st.number_input("Enter Thickness (Min:0.18 & Max:400)")
-                                 width = st.number_input("Enter Width (Min:1 & Max:2990)")
-                                 customer = st.number_input("Enter Customer ID (Min:12458 & Max:30408185)")
-                                 submitted = st.form_submit_button(label = "PREDICT SELLING PRICE")
-                                  
-                                  
-                                 if submitted:
-
-                                      with open("models/cmodel.pkl", 'rb') as file:
-                                        loaded_model = pickle.load(file)
-                                      #with open("models/cscaler.pkl", 'rb') as f:
-                                        #scaler_loaded = pickle.load(f)
-                                      #with open("models/ct.pkl", 'rb') as f:
-                                        #t_loaded = pickle.load(f)
-                                      #with open(r"models/s.pkl", 'rb') as f:
-                                          #s_loaded = pickle.load(f)
+                          st.write(' ')
+                          status = st.selectbox("Status", status_options,key=1)
+                          item_type = st.selectbox("Item Type", item_type_options,key=2)
+                          country = st.selectbox("Country", sorted(country_options),key=3)
+                          application = st.selectbox("Application", sorted(application_options),key=4)
+                          product_ref = st.selectbox("Product Reference", product,key=5)
+                      with col3:               
+                          st.write( f'<h5 style="color:rgb(0, 153, 153,0.4);">NOTE: Min & Max given for reference, you can enter any value</h5>', unsafe_allow_html=True )
+                          quantity_tons = st.text_input("Enter Quantity Tons (Min:611728 & Max:1722207579)")
+                          thickness = st.text_input("Enter thickness (Min:0.18 & Max:400)")
+                          width = st.text_input("Enter width (Min:1, Max:2990)")
+                          customer = st.text_input("customer ID (Min:12458, Max:30408185)")
+                          submit_button = st.form_submit_button(label="PREDICT SELLING PRICE")
+                          st.markdown("""
+                              <style>
+                              div.stButton > button:first-child {
+                                  background-color: #009999;
+                                  color: white;
+                                  width: 100%;
+                              }
+                              </style>
+                          """, unsafe_allow_html=True)
               
-                                      # -----Sending that data to the trained models for selling price prediction-----
-                                      new_sample = np.array([[np.log(float(quantity_tons)), application, np.log(float(thickness)),
-                                                              float(width), country, float(customer), int(product_ref), item_type,
-                                                              status]])
-                                      #new_sample_ohe = t_loaded.transform(new_sample[:, [7]]).toarray()
-                                      #new_sample_be = s_loaded.transform(new_sample[:, [8]]).toarray()
-                                      #new_sample = np.concatenate(
-                                          #(new_sample[:, [0, 1, 2, 3, 4, 5, 6]], new_sample_ohe, new_sample_be),
-                                          #axis=1)
-                                      #new_sample1 = scaler_loaded.transform(new_sample)
-                                      new_pred = loaded_model.predict(new_sample)[0]
-              
-                                      # Used np.log earlier to handle data discrepancies, so to get the real output using np.exp
-                                      st.write('## :green[Predicted selling price:] ', np.exp(new_pred))
-                                      #predict_text ='''<h5 style='font_size: 4px; text-align: left; color: green;' > Selling Price </h5'''
-                                      #st.markdown(predict_text, unsafe_allow_html=True)
+                      flag=0 
+                      pattern = "^(?:\d+|\d*\.\d+)$"
+                      for i in [quantity_tons,thickness,width,customer]:             
+                          if re.match(pattern, i):
+                              pass
+                          else:                    
+                              flag=1  
+                              break
+                      
+                  if submit_button and flag==1:
+                      if len(i)==0:
+                          st.write("please enter a valid number space not allowed")
+                      else:
+                          st.write("You have entered an invalid value: ",i)  
+                       
+                  if submit_button and flag==0:
+                      
+                      import pickle
+                      with open(r"source/model.pkl", 'rb') as file:
+                          loaded_model = pickle.load(file)
+                      with open(r'source/scaler.pkl', 'rb') as f:
+                          scaler_loaded = pickle.load(f)
+          
+                      with open(r"source/t.pkl", 'rb') as f:
+                          t_loaded = pickle.load(f)
+          
+                      with open(r"source/s.pkl", 'rb') as f:
+                          s_loaded = pickle.load(f)
+          
+                      new_sample= np.array([[np.log(float(quantity_tons)),application,np.log(float(thickness)),float(width),country,float(customer),int(product_ref),item_type,status]])
+                      new_sample_ohe = t_loaded.transform(new_sample[:, [7]]).toarray()
+                      new_sample_be = s_loaded.transform(new_sample[:, [8]]).toarray()
+                      new_sample = np.concatenate((new_sample[:, [0,1,2, 3, 4, 5, 6,]], new_sample_ohe, new_sample_be), axis=1)
+                      new_sample1 = scaler_loaded.transform(new_sample)
+                      new_pred = loaded_model.predict(new_sample1)[0]
+                      st.write('## :green[Predicted selling price:] ', np.exp(new_pred))
       
     with tab2:
       
-              with st.form("my form 2"): 
-                     
-                      col4,col5,col6 = st.columns([5,2,5])
-                      with col4:
-                                 st.write (" ")
-                                 status =  st.selectbox("Status",status_values,key =6)
-                                 item_type =  st.selectbox("Item Type",item_type_values, key =7)
-                                 country =  st.selectbox("Country",country_values, key =8)
-                                 application =  st.selectbox("Application",application_values, key =9)
-                                 product_ref =  st.selectbox("Product Reference",product_ref_values, key =10)
-                      with col6:
-                                 #st.write(f'<h5 style="color:rgb(0, 153, 153, 0.4);">NOTE: Min & Max given for reference, y)
-                                 quantity_tons = st.text_input("Enter Quantity Tons (Min:611728 & Max:1722207579)")
-                                 thickness = st.text_input("Enter Thickness (Min:0.18 & Max:400)")
-                                 width = st.text_input("Enter Width (Min:1 & Max:2990)")
-                                 customer = st.text_input("Enter Customer ID (Min:12458 & Max:30408185)")
-                                 submitted = st.form_submit_button(label = "PREDICT SELLING PRICE")
-                                 #if submitted:
-                                 #st.write(f"Predicting the price for: ok ") #{brand} {model} ({year}), Mileage: {mileage} km.")
-                                 st.markdown("""
-                                           <style>
-                                            div.stButton > button:first-child {
-                                             background-color: #009999;
-                                             color: white;
-                                             width: 100%;
-                                           }
-                                           </sytle>
-                                           """, unsafe_allow_html=True)   
-                                  
-                      if submitted:
-                                      predict_text ='''<h5 style='font_size: 4px; text-align: left; color: green;' > Status </h5'''
-                                      st.markdown(predict_text, unsafe_allow_html=True)
+                with st.form("my_form1"):
+                        col1,col2,col3=st.columns([5,1,5])
+                        with col1:
+                            cquantity_tons = st.text_input("Enter Quantity Tons (Min:611728 & Max:1722207579)")
+                            cthickness = st.text_input("Enter thickness (Min:0.18 & Max:400)")
+                            cwidth = st.text_input("Enter width (Min:1, Max:2990)")
+                            ccustomer = st.text_input("customer ID (Min:12458, Max:30408185)")
+                            cselling = st.text_input("Selling Price (Min:1, Max:100001015)") 
+                          
+                        with col3:    
+                            st.write(' ')
+                            citem_type = st.selectbox("Item Type", item_type_options,key=21)
+                            ccountry = st.selectbox("Country", sorted(country_options),key=31)
+                            capplication = st.selectbox("Application", sorted(application_options),key=41)  
+                            cproduct_ref = st.selectbox("Product Reference", product,key=51)           
+                            csubmit_button = st.form_submit_button(label="PREDICT STATUS")
+                
+                        cflag=0 
+                        pattern = "^(?:\d+|\d*\.\d+)$"
+                        for k in [cquantity_tons,cthickness,cwidth,ccustomer,cselling]:             
+                            if re.match(pattern, k):
+                                pass
+                            else:                    
+                                cflag=1  
+                                break
+                        
+                    if csubmit_button and cflag==1:
+                        if len(k)==0:
+                            st.write("please enter a valid number space not allowed")
+                        else:
+                            st.write("You have entered an invalid value: ",k)  
+                         
+                    if csubmit_button and cflag==0:
+                        import pickle
+                        with open(r"source/cmodel.pkl", 'rb') as file:
+                            cloaded_model = pickle.load(file)
+            
+                        with open(r'source/cscaler.pkl', 'rb') as f:
+                            cscaler_loaded = pickle.load(f)
+            
+                        with open(r"source/ct.pkl", 'rb') as f:
+                            ct_loaded = pickle.load(f)
+            
+                        # Predict the status for a new sample
+                        # 'quantity tons_log', 'selling_price_log','application', 'thickness_log', 'width','country','customer','product_ref']].values, X_ohe
+                        new_sample = np.array([[np.log(float(cquantity_tons)), np.log(float(cselling)), capplication, np.log(float(cthickness)),float(cwidth),ccountry,int(ccustomer),int(product_ref),citem_type]])
+                        new_sample_ohe = ct_loaded.transform(new_sample[:, [8]]).toarray()
+                        new_sample = np.concatenate((new_sample[:, [0,1,2, 3, 4, 5, 6,7]], new_sample_ohe), axis=1)
+                        new_sample = cscaler_loaded.transform(new_sample)
+                        new_pred = cloaded_model.predict(new_sample)
+                        if new_pred==1:
+                            st.write('## :green[The Status is Won] ')
+                        else:
+                            st.write('## :red[The status is Lost] ')
